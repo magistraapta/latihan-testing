3eabfaa1d25cbb344925a561d10c2f5bfbb3a465
magistraapta <magsitraagis@gmail.com>
2024-01-25 13:00:43 +0800

    Initial Commit

---
+++ latihan-testing/latihan_testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  latihan_testingApp.swift
+//  latihan-testing
+//
+//  Created by Magistra Apta on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct latihan_testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ latihan-testingUITests/latihan_testingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  latihan_testingUITestsLaunchTests.swift
+//  latihan-testingUITests
+//
+//  Created by Magistra Apta on 25/01/24.
+//
+
+import XCTest
+
+final class latihan_testingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
